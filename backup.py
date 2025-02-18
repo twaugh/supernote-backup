3667a863da84ab008b7f2644184bd4512032cf1f
Tim Waugh <twaugh@redhat.com>
2025-02-18 16:42:06 +0000

    Handle filenames containing '/'

--- backup.py
+++ backup.py
@@ -2,9 +2,11 @@
 import hashlib
 import logging
 import os
+import string
 import subprocess
 import sys
 import tempfile
+import urllib.parse
 
 
 logging.basicConfig(level=logging.INFO)
@@ -14,6 +16,10 @@ import supernote
 
 
 class Supernote(object):
+    # We need to percent-encode "/" to describe remote paths, since we're representing
+    # folders like a filesystem with "/" as a separator
+    _SAFE_CHARS = " \t" + "".join(set(string.punctuation) - {"/"})
+
     def __init__(self, username, password):
         self._token = supernote.login(username, password)
 
@@ -25,10 +31,19 @@ class Supernote(object):
         # Also cache file information
         self._id_to_stat = {}
 
+    @classmethod
+    def quote(cls, path):
+        return urllib.parse.quote(path, safe=cls._SAFE_CHARS)
+
+    @staticmethod
+    def unquote(path):
+        return urllib.parse.unquote(path)
+
     def _cache_id(self, path, ident):
         ident = int(ident)
         self._id_to_path[ident] = path
         self._path_to_id[path] = ident
+        log.info(self._path_to_id)
 
     def file_list(self, ident=0):
         entries = supernote.file_list(self._token, ident)
@@ -40,7 +55,8 @@ class Supernote(object):
             # Cache identifiers from the result
             for entry in entries:
                 self._cache_id(
-                    os.path.join(current_path, entry["fileName"]), entry["id"]
+                    os.path.join(current_path, Supernote.quote(entry["fileName"])),
+                    entry["id"],
                 )
 
         # Cache file information
@@ -51,48 +67,73 @@ class Supernote(object):
 
         return entries
 
-    def stat(self, path=None, ident=0):
-        if path is not None:
-            ident = self._path_to_id[path]
+    def stat_path(self, path):
+        ident = self._path_to_id[path]
+        return self.stat_id(ident)
 
+    def stat_id(self, ident):
         return self._id_to_stat[int(ident)]
 
-    def download_file(self, filename, path=None, ident=0):
-        if path is not None:
-            ident = self._path_to_id[path]
+    def upload_file_path(self, src, dstpath):
+        try:
+            dstident = self._path_to_id[dstpath]
+        except KeyError:
+            raise RuntimeError(
+                "Destination path not previously seen, use identifer instead"
+            )
 
+        return self.upload_file_id(self, src, ident=dstident)
+
+    def upload_file_id(self, src, ident=0):
+        return supernote.upload_file(self._token, src, directory=dstident)
+
+    def download_file_path(self, path, filename):
+        ident = self._path_to_id[path]
+        return self.download_file_id(ident, filename)
+
+    def download_file_id(self, ident, filename):
         return supernote.download_file(self._token, ident, filename)
 
-    def walk(self, path="", ident=0):
-        if path and ident:
-            raise RuntimeError("Both path and identifier specified")
+    def walk_path(self, path=""):
+        try:
+            ident = self._path_to_id[path]
+        except KeyError:
+            raise RuntimeError("Path not previously seen, use identifier instead")
 
-        if not ident:
-            try:
-                ident = self._path_to_id[path]
-            except KeyError:
-                raise RuntimeError("Path not previously seen, use identifier instead")
+        return self.walk_id(self, ident=ident)
 
+    def walk_id(self, ident=0):
         # List what's in a folder
         entries = self.file_list(ident=ident)
 
         # Separate folders from files
         folders = []
         files = []
-        current_path = path or self._id_to_path[int(ident)]
+        current_path = self._id_to_path[int(ident)]
         for entry in entries:
             if entry["isFolder"] == "Y":
                 folders.append(entry)
             else:
                 files.append(entry)
 
-        foldernames = [folder["fileName"] for folder in folders]
-        yield (current_path, foldernames, [file["fileName"] for file in files])
+        foldernames = [Supernote.quote(folder["fileName"]) for folder in folders]
+        yield (
+            current_path,
+            foldernames,
+            [Supernote.quote(file["fileName"]) for file in files],
+        )
 
         # Trim folders in case foldernames was modified by caller
-        folders = [folder for folder in folders if folder["fileName"] in foldernames]
+        folders = [
+            folder
+            for folder in folders
+            if Supernote.quote(folder["fileName"]) in foldernames
+        ]
         for folder in folders:
-            yield from self.walk(ident=folder["id"])
+            yield from self.walk_id(ident=folder["id"])
+
+    def walk(self):
+        return self.walk_id()
 
 
 def calculate_md5sum(filename):
@@ -129,7 +170,7 @@ def sync(sn, destdir):
 
         for file in files:
             filename = os.path.join(path, file)
-            stat = sn.stat(filename)
+            stat = sn.stat_path(filename)
             destfile = os.path.join(destdir, filename)
             if "md5" in stat:
                 try:
@@ -142,7 +183,7 @@ def sync(sn, destdir):
                         continue
 
             log.info(f"Downloading {filename}")
-            sn.download_file(destfile, path=filename)
+            sn.download_file_path(filename, destfile)
 
 
 def main():
