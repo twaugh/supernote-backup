61d21031f55d2375c968ad6104ab497d95730fc1
Tim Waugh <twaugh@redhat.com>
2025-02-18 14:28:14 +0000

    Walk-like implementation

--- backup.py
+++ backup.py
@@ -6,7 +6,7 @@ import sys
 import tempfile
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 sys.path.append("./supernote-cloud-python")
 import supernote
@@ -15,36 +15,74 @@ import supernote
 class Supernote(object):
     def __init__(self, username, password):
         self._token = supernote.login(username, password)
+        self._id_to_path = {
+            0: "",
+        }
+        self._path_to_id = {
+            "": 0,
+        }
 
-    def file_list(self, directory=0):
-        return supernote.file_list(self._token, directory)
+    def _cache_id(self, path, ident):
+        ident = int(ident)
+        self._id_to_path[ident] = path
+        self._path_to_id[path] = ident
 
-    def download_file(self, ident, filename):
+    def file_list(self, ident=0):
+        entries = supernote.file_list(self._token, ident)
+        try:
+            current_path = self._id_to_path[int(ident)]
+        except KeyError:
+            pass
+        else:
+            # Cache identifiers from the result
+            for entry in entries:
+                self._cache_id(
+                    os.path.join(current_path, entry["fileName"]), entry["id"]
+                )
+
+        return entries
+
+    def download_file(self, filename, path=None, ident=0):
+        if path is not None:
+            ident = self._path_to_id[path]
+
         return supernote.download_file(self._token, ident, filename)
 
+    def walk(self, path="", ident=0):
+        if path and ident:
+            raise RuntimeError("Both path and identifier specified")
 
-def recurse_file_tree(sn, file_list):
-    for entry in file_list:
-        fileName = entry["fileName"]
-        if entry["isFolder"] == "Y":
+        if not ident:
             try:
-                os.mkdir(fileName)
-            except FileExistsError:
-                pass
+                ident = self._path_to_id[path]
+            except KeyError:
+                raise RuntimeError("Path not previously seen, use identifier instead")
 
-            os.chdir(fileName)
-            log.debug(f"recurse into {fileName}")
-            subdir = sn.file_list(directory=entry["id"])
-            recurse_file_tree(sn, subdir)
-        else:
-            log.debug(f"download {fileName}")
-            with tempfile.NamedTemporaryFile(dir=".", delete=False) as tmpf:
-                sn.download_file(entry["id"], tmpf.name)
-                os.rename(tmpf.name, fileName)
+        # List what's in a folder
+        entries = self.file_list(ident=ident)
+
+        # Separate folders from files
+        folders = []
+        files = []
+        current_path = path or self._id_to_path[int(ident)]
+        for entry in entries:
+            if entry["isFolder"] == "Y":
+                folders.append(entry)
+            else:
+                files.append(entry)
+
+        foldernames = [folder["fileName"] for folder in folders]
+        yield (current_path, foldernames, [file["fileName"] for file in files])
+
+        # Trim folders in case foldernames was modified by caller
+        folders = [folder for folder in folders if folder["fileName"] in foldernames]
+        for folder in folders:
+            yield from self.walk(ident=folder["id"])
 
 
 def main():
     username = sys.argv[1]
+    destdir = sys.argv[2]
     sp = subprocess.Popen(
         ["pass", f"supernote/{username}"],
         stdout=subprocess.PIPE,
@@ -58,8 +96,25 @@ def main():
     password = stdout.splitlines()[0].decode()
     sn = Supernote(username, password)
 
-    top = sn.file_list()
-    recurse_file_tree(sn, top)
+    # mkdir -p
+    components = os.path.split(destdir)
+    for ncomp in range(len(components)):
+        dir = os.path.join(*components[: ncomp + 1])
+        if not dir:
+            continue
+        try:
+            os.mkdir(dir)
+        except FileExistsError:
+            pass
+
+    for path, dirs, files in sn.walk():
+        if path:
+            os.mkdir(os.path.join(destdir, path))
+
+        for file in files:
+            filename = os.path.join(path, file)
+            log.info(f"Downloading {filename}")
+            sn.download_file(os.path.join(destdir, filename), path=filename)
 
 
 if __name__ == "__main__":
